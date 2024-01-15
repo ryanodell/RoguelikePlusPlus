9f8c17ea5350ae0f7793166033257ea15ad4ede4
Ryan ODell <ryanodell2013@gmail.com>
2024-01-15 15:25:02 -0800

    mostly finished abstracting away game window

--- RLOpengGL/myimplementation.cpp
+++ RLOpengGL/myimplementation.cpp
@@ -3,25 +3,16 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-void processInput(GLFWwindow *window);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-
 int main() {
-    Init();
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
+    GameWindow gameWindow = GameWindow(SCR_WIDTH, SCR_HEIGHT, "Test");
+    if(gameWindow.GetWindow() == nullptr) {
         return -1;
     }
-    glfwMakeContextCurrent(window);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     float vertices[] = {
          0.5f,  0.5f, 0.0f,  // top right
          0.5f, -0.5f, 0.0f,  // bottom right
@@ -33,23 +24,13 @@ int main() {
         1, 2, 3   // second Triangle
     };
 
-    //VertexBuffer vert = VertexBuffer(vertices, 4 * 2 * sizeof(float));
-    while (!glfwWindowShouldClose(window)) {
-        processInput(window);
+    VertexBuffer vert = VertexBuffer(vertices, 4 * 2 * sizeof(float));
+    while (!glfwWindowShouldClose(gameWindow.GetWindow())) {
+        gameWindow.Update();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        gameWindow.SwapBuffers();
+        gameWindow.PollEvents();
     }
-    glfwTerminate();
     return 0;
-}
-
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
 }
--- RLOpengGL/myimplementation.h
+++ RLOpengGL/myimplementation.h
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+////////////////////////////////VERTEX BUFFER//////////////////////////////////
 class VertexBuffer {
 public:
     VertexBuffer(const void* data, unsigned int size);
@@ -14,30 +15,78 @@ public:
 private:
     unsigned int m_VertexBufferId;
 };
-
 VertexBuffer::VertexBuffer(const void* data, unsigned int size) {
     glGenBuffers(1, &m_VertexBufferId);
     glBindBuffer(GL_ARRAY_BUFFER, m_VertexBufferId);
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 }
-
 VertexBuffer::~VertexBuffer() {
     glDeleteBuffers(1, &m_VertexBufferId);
 }
-
 void VertexBuffer::Bind() {
     glBindBuffer(GL_ARRAY_BUFFER, m_VertexBufferId);
 }
-
 void VertexBuffer::Unbind() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
+////////////////////////////////VERTEX BUFFER//////////////////////////////////
 
-void Init(){
+
+////////////////////////////////GAME WINDOW//////////////////////////////////
+class GameWindow {
+public:
+    GameWindow(unsigned int windowWidth, unsigned int windowHeight, const char* windowTitle);
+    ~GameWindow();
+    bool IsRunning();
+    void Update();
+    void SwapBuffers();
+    void PollEvents();
+    GLFWwindow* GetWindow() const { return m_window; };
+private:
+    GLFWwindow* m_window;
+    unsigned int m_width;
+    unsigned int m_height;
+    const char* m_windowTitle;
+    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+};
+GameWindow::GameWindow(unsigned int windowWidth, unsigned int windowHeight, const char* windowTitle) {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    m_width = windowWidth;
+    m_height = windowHeight;
+    m_windowTitle = windowTitle;
+    m_window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, NULL, NULL);
+    if (m_window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(m_window);
+    glfwSetFramebufferSizeCallback(m_window, GameWindow::framebuffer_size_callback);
 }
+GameWindow::~GameWindow() {
+    glfwTerminate();
+}
+void GameWindow::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    GameWindow* gameWindow = static_cast<GameWindow*>(glfwGetWindowUserPointer(window));
+    if (gameWindow != nullptr) {
+        gameWindow->m_width = width;
+        gameWindow->m_height = height;
+        glViewport(0, 0, width, height);
+    }
+}
+void GameWindow::SwapBuffers() {
+    glfwSwapBuffers(m_window);
+}
+void GameWindow::PollEvents() {
+    glfwPollEvents();
+}
+void GameWindow::Update() {
+   if (glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(m_window, true);
+}
+////////////////////////////////GAME WINDOW//////////////////////////////////
+
 
 #endif // MYIMPLEMENTATION_H_INCLUDED
