c15a6cd2b2ece8fd5b59c84ed6c3f87f256c3b2b
Ryan ODell <ryanodell2013@gmail.com>
2024-01-29 15:58:53 -0800

    Added the command as comment

--- RLOpengGLVSCODE/main.cpp
+++ RLOpengGLVSCODE/main.cpp
@@ -1,6 +1,8 @@
+//g++ main.cpp -IC:\ExternalLibs\SDL2\include\SDL2 -LC:\ExternalLibs\SDL2\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o main
+//#include <SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Fuck" << std::endl;
+int main(int argc, char* args[]) {
+    printf("Fuck you\n");
     return 0;
 }
