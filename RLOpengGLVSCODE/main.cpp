c20ce2b3aea3832d8b0b5b9626cf22b67efe11a7
Ryan ODell <ryanodell2013@gmail.com>
2024-01-29 16:18:15 -0800

    IT WORKSgit status!

--- RLOpengGLVSCODE/main.cpp
+++ RLOpengGLVSCODE/main.cpp
@@ -1,8 +1,50 @@
 //g++ main.cpp -IC:\ExternalLibs\SDL2\include\SDL2 -LC:\ExternalLibs\SDL2\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o main
-//#include <SDL.h>
+// g++ main.cpp -IC:\ExternalLibs\SDL2\include\SDL2 -LC:\ExternalLibs\SDL2\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o main
+// g++ main.cpp -IC:\ExternalLibs\SDL2\include\SDL2 -LC:\ExternalLibs\SDL2\lib -w -lmingw32 -lSDL2main -lSDL2 -o main
+// g++ main.cpp -IC:\ExternalLibs\SDL2\include\SDL2 -LC:\ExternalLibs\SDL2\lib -w -Wl,-subsystem,windows -lmingw32 -lSDL2main -lSDL2 -o main
+
+#include <SDL.h>
+#include <windows.h>
 #include <iostream>
 
-int main(int argc, char* args[]) {
-    printf("Fuck you\n");
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    //The window we'll be rendering to
+	SDL_Window* window = NULL;
+	
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( window == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface( window );
+
+			//Fill the surface white
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			
+			//Update the surface
+			SDL_UpdateWindowSurface( window );
+            
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+    }
     return 0;
 }
