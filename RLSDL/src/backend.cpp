a82292a1a1b6723c5b548ee86cd2925d1573fd60
Ryan ODell <ryanodell2013@gmail.com>
2024-02-04 11:31:39 -0800

    Added some structure for textures and loading - not tested

--- RLSDL/src/backend.cpp
+++ RLSDL/src/backend.cpp
@@ -1,12 +1,11 @@
 #include "backend.h"
 
-
+///////////////////////////GAME////////////////////////////////////////
 Game::Game(int width, int height, const char* windowTitle) : 
             mWidth(width), 
             mHeight(height), 
             mWindowTitle(windowTitle) {    
 }
-
 Game::~Game() {
     SDL_DestroyWindow(mWindow);
     SDL_DestroyRenderer(mRenderer);
@@ -15,7 +14,6 @@ Game::~Game() {
     IMG_Quit();
     SDL_Quit();
 }
-
 bool Game::Init() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("There was an error initializing SDL || SDL_Error: %s\n", SDL_GetError());
@@ -40,6 +38,58 @@ bool Game::Init() {
 	}
     return true;
 }
-
 void Game::Run() {
+    TextureManager textureManager;
+
 }
+///////////////////////////END GAME////////////////////////////////////////
+
+///////////////////////////TEXTURE2D////////////////////////////////////////
+Texture2D::Texture2D(SDL_Texture* texture) {
+    mInternalTexture = texture;
+}
+
+Texture2D::~Texture2D() {
+    if(mInternalTexture != NULL) {
+        SDL_DestroyTexture(mInternalTexture);
+        mInternalTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+    }
+}
+///////////////////////////END TEXTURE2D////////////////////////////////////////
+
+
+///////////////////////////TEXTUREMANAGER////////////////////////////////////////
+void TextureManager::Init(SDL_Renderer *renderer) {
+    //mRenderer = &renderer;
+}
+
+Texture2D* TextureManager::LoadTexture(const char* name) {
+    auto it = mTextureCache.find(name);
+    if(it == mTextureCache.end()) {
+        if(_loadTextureCache(name) == false) {            
+             printf("Failed to load %s into the texture cache\n", name);            
+            return nullptr;
+        } 
+    }
+    return mTextureCache[name].get();
+}
+bool TextureManager::_loadTextureCache(const char *name) {
+    SDL_Texture* newTexture;
+    SDL_Surface* loadedSurface = IMG_Load(name);
+    if(loadedSurface == NULL) {
+        printf("Failed to load surface | IMG_Error: %s\n", IMG_GetError());
+        return false;
+    } else {
+        newTexture = SDL_CreateTextureFromSurface(mRenderer, loadedSurface);
+        if(newTexture == NULL) {
+            printf("Failed to create texture from surface | SDL_Error: %s\n", SDL_GetError());
+            return false;
+        } else {
+            mTextureCache[name] = std::make_shared<Texture2D>(newTexture);
+            SDL_FreeSurface(loadedSurface);
+        }
+    }
+}
+///////////////////////////END TEXTUREMANAGER////////////////////////////////////////
--- RLSDL/src/backend.h
+++ RLSDL/src/backend.h
@@ -2,6 +2,8 @@
 #define BACKEND_H
 #include <stdio.h>
 #include <assert.h>
+#include <unordered_map>
+#include <memory>
 #include <SDL.h>
 #include <SDL_image.h>
 
@@ -11,8 +13,9 @@ public:
     ~Game();
     bool Init();
     void Run();
-    int GetWidth() { return mWidth; };
-    int GetHeight() { return mHeight; };
+    int GetWidth() const { return mWidth; };
+    int GetHeight() const { return mHeight; };
+    SDL_Renderer* GetRenderer() const { return mRenderer; }
 private:
     SDL_Window* mWindow;
     SDL_Renderer* mRenderer;
@@ -20,5 +23,30 @@ private:
     int mHeight;
     const char* mWindowTitle;
 };
+
+class Texture2D {
+public:
+    int GetWidt() const { return mWidth; };
+    int GetHeight() const { return mHeight; };
+    Texture2D(SDL_Texture* texture);
+    ~Texture2D();
+private:
+    SDL_Texture* mInternalTexture;
+    int mWidth;
+    int mHeight;
+};
+
+class TextureManager {
+public:
+    void Init(SDL_Renderer* renderer);
+    Texture2D* LoadTexture(const char *name);
+private:
+    // std::unordered_map<const char*, Texture2D*> mTextureCache;
+    std::unordered_map<const char*, std::shared_ptr<Texture2D>> mTextureCache;
+    //std::unordered_map<const char*, std::shared_ptr<Texture2D>> classMap;
+    bool _loadTextureCache(const char* name);
+    SDL_Renderer* mRenderer;
+};
+
 
 #endif
