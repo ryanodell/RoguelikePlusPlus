0f9c09e3a161d36b7cd3ecfb9a05fb88a10eb821
Ryan ODell <ryanodell2013@gmail.com>
2024-02-10 11:58:11 -0800

    Updated prints to use logger

--- RLSDL/src/backend.cpp
+++ RLSDL/src/backend.cpp
@@ -16,24 +16,24 @@ Game::~Game() {
 }
 bool Game::Init() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("There was an error initializing SDL || SDL_Error: %s\n", SDL_GetError());
+        Logger::LogError("There was an error initializing SDL || SDL_Error: %s\n", SDL_GetError());
         return false;
     }
     mWindow = SDL_CreateWindow(mWindowTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
         mWidth, mHeight, SDL_WINDOW_SHOWN);
     if(mWindow == NULL) {
-        printf("Failed to initialize Window | SDL_Error: %s\n", SDL_GetError());
+        Logger::LogError("Failed to initialize Window | SDL_Error: %s\n", SDL_GetError());
         return false;
     }
     mRenderer = SDL_CreateRenderer(mWindow, 01, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(mRenderer == NULL) {
-        printf("Failed to initialize SDL Renderer | SDL_Error: %s\n", SDL_GetError());
+        Logger::LogError("Failed to initialize SDL Renderer | SDL_Error: %s\n", SDL_GetError());
         return false;
     }
     SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
     int imgFlags = IMG_INIT_PNG;
 	if( !( IMG_Init( imgFlags ) & imgFlags ) ) {
-		printf("SDL_Image could not be initialized. SDL_Image Error: %s\n", IMG_GetError());
+		Logger::LogError("SDL_Image could not be initialized. SDL_Image Error: %s\n", IMG_GetError());
         return false;
 	}
     return true;
@@ -127,7 +127,7 @@ Texture2D* TextureManager::LoadTexture(const char* name) {
     auto it = mTextureCache.find(name);
     if(it == mTextureCache.end()) {
         if(_loadTextureCache(name) == false) {            
-             printf("Failed to load %s into the texture cache\n", name);            
+             Logger::LogError("Failed to load %s into the texture cache\n", name);            
             return nullptr;
         } 
     }
@@ -137,7 +137,7 @@ bool TextureManager::_loadTextureCache(const char *name) {
     SDL_Texture* newTexture;
     SDL_Surface* loadedSurface = IMG_Load(name);
     if(loadedSurface == NULL) {
-        printf("Failed to load surface | IMG_Error: %s\n", IMG_GetError());
+        Logger::LogError("Failed to load surface | IMG_Error: %s\n", IMG_GetError());
         return false;
     } else {
         SDL_Surface* formattedSurface = SDL_ConvertSurfaceFormat(loadedSurface, SDL_PIXELFORMAT_RGBA8888, 0);
@@ -145,13 +145,14 @@ bool TextureManager::_loadTextureCache(const char *name) {
         SDL_SetColorKey(formattedSurface, SDL_TRUE, magentaColor);
         SDL_Texture* texture = SDL_CreateTextureFromSurface(mRenderer, formattedSurface);
         if(texture == NULL) {
-            printf("Failed to create texture from surface | SDL_Error: %s\n", SDL_GetError());
+            Logger::LogError("Failed to create texture from surface | SDL_Error: %s\n", SDL_GetError());
             return false;
         }
         mTextureCache[name] = std::make_shared<Texture2D>(texture, loadedSurface->w, loadedSurface->h);
         SDL_FreeSurface(loadedSurface);
-        SDL_FreeSurface(formattedSurface);
+        SDL_FreeSurface(formattedSurface);        
     }
+    return true;
 }
 
 ///////////////////////////END TEXTUREMANAGER////////////////////////////////////////
--- RLSDL/src/main.cpp
+++ RLSDL/src/main.cpp
@@ -5,12 +5,10 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    Logger::LogInfo("Hello %s ", "t");
-    Logger::LogInfo("Hello %s", "test arg");
-    Logger::LogInfo("Test");
+    Logger::LogInfo("..STARTING..");
     Game game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, "RLSDL");
     if(!game.Init()) {
-        printf("There was an error during initialization process. Check Logs");
+        Logger::LogError("There was an error during initialization process. Check Logs");
         return -1;
     }
     game.Run();
