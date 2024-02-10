53b2d112006bd644ffebcd29d42ec39ac711a1d0
Ryan ODell <ryanodell2013@gmail.com>
2024-02-10 05:54:52 -0800

    Reformatted MAKEFILE, Updated VSCode to launch console

--- RLSDL/src/main.cpp
+++ RLSDL/src/main.cpp
@@ -5,6 +5,7 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    printf("..STARTING..");
     Game game = Game(SCREEN_WIDTH, SCREEN_HEIGHT, "RLSDL");
     if(!game.Init()) {
         printf("There was an error during initialization process. Check Logs");
