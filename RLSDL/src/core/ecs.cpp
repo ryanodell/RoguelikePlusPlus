23b009857fb0c9625d425e6cc14368b5438bfbdd
Ryan ODell <ryanodell2013@gmail.com>
2024-02-14 09:56:35 -0800

    moved component manager over - buildable

--- RLSDL/src/core/ecs.cpp
+++ RLSDL/src/core/ecs.cpp
@@ -101,27 +101,27 @@ void EntityManger::DestroyEntity(Entity entity) {
 //     ++mNextComponentType;
 // }
 
-template <typename T>
-ComponentType ComponentManager::GetComponentType() {
-    const char* typeName = typeid(T).name();
-    //assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");	
-	return mComponentTypes[typeName];
-}
+// template <typename T>
+// ComponentType ComponentManager::GetComponentType() {
+//     const char* typeName = typeid(T).name();
+//     //assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");	
+// 	return mComponentTypes[typeName];
+// }
 
-template <typename T>
-void ComponentManager::AddComponent(Entity entity, T component) {
-    GetComponentArray<T>()->InsertData(entity, component);
-}
+// template <typename T>
+// void ComponentManager::AddComponent(Entity entity, T component) {
+//     GetComponentArray<T>()->InsertData(entity, component);
+// }
 
-template <typename T>
-void ComponentManager::RemoveComponent(Entity entity) {
-    GetComponentArray<T>()->RemoveData(entity);
-}
+// template <typename T>
+// void ComponentManager::RemoveComponent(Entity entity) {
+//     GetComponentArray<T>()->RemoveData(entity);
+// }
 
-template <typename T>
-T &ComponentManager::GetComponent(Entity entity) {
-    return GetComponentArray<T>()->GetData(entity);
-}
+// template <typename T>
+// T &ComponentManager::GetComponent(Entity entity) {
+//     return GetComponentArray<T>()->GetData(entity);
+// }
 
 void ComponentManager::EntityDestroyed(Entity entity) {
     for(auto const& pair : mComponentArrays) {        
--- RLSDL/src/core/ecs.h
+++ RLSDL/src/core/ecs.h
@@ -94,14 +94,27 @@ public:
          mComponentArrays.insert({typeName, std::make_shared<ComponentArray<T>>()});
         ++mNextComponentType;
     };
-    template<typename T>
-    ComponentType GetComponentType();
-    template<typename T>
-    void AddComponent(Entity entity, T component);
-    template<typename T>
-    void RemoveComponent(Entity entity);
-    template<typename T>
-    T& GetComponent(Entity entity);
+    template <typename T>
+    ComponentType GetComponentType() {
+        const char* typeName = typeid(T).name();
+        //assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");	
+    	return mComponentTypes[typeName];
+    };
+
+    template <typename T>
+    void AddComponent(Entity entity, T component) {
+        GetComponentArray<T>()->InsertData(entity, component);
+    };
+
+    template <typename T>
+    void RemoveComponent(Entity entity) {
+        GetComponentArray<T>()->RemoveData(entity);
+    };
+
+    template <typename T>
+    T& GetComponent(Entity entity) {
+        return GetComponentArray<T>()->GetData(entity);
+    };
     void EntityDestroyed(Entity entity);
 
 private:
